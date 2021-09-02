b14e9c6e2f04144ca5ba5683cb957e6c6e2aaebc
motiurrahmanmisu <motiurrahmanmisu@gmail.com>
2021-09-02 15:08:25 +0600

    book details

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const searchBox = document.getElementById("search-input")
+
+document.getElementById("search-btn").addEventListener("click", () => {
+    search = searchBox.value;
+    searchBox.value = ""
+    const url = `https://openlibrary.org/search.json?q=${search}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayResult(data.docs))
+})
+
+
+const displayResult = books => {
+    const bookContainer = document.getElementById("book-container")
+    bookContainer.textContent = ""
+    let count = 0;
+    books.forEach(book => {
+        document.getElementById("count-book").innerText = "Total Book Found: " + count++;
+        console.log(book);
+        const div = document.createElement("div")
+        div.classList.add("col")
+        div.innerHTML = `
+        <div class="card h-100">
+        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="..." height="450px">
+        <div class="card-body">
+            <h5 class="card-title">${book.title}</h5>
+            <h6>Author Name: ${book.author_name[0]}</h6>
+            <h6>First Published: ${book.first_publish_year}</h6>
+        </div>
+    </div>
+        `
+        bookContainer.appendChild(div)
+    });
+}
+
+// function authorKey(authorkey) {
+//     fetch(`https://openlibrary.org/authors/${authorkey}.json`)
+//         .then(res => res.json())
+//         .then(data => console.log(data.name))
+// }
+
+
+// function displayAuthor(authorName) {
+//     return authorName.name;
+// }
